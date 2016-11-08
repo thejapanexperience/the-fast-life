578998476682046f733b1e69eb643bafa02d7f4c
Cade Nichols <c@codinghouse.co>
2016-11-08 04:02:49 -0800

    init

---
+++ src/components/Layout.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1 className='text-center'>Redux Express</h1>
+      </div>
+    )
+  }
+}
