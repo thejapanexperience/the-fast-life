e9595feaf9e12a33140d664faa95746bc2233133
Richard Mands <richard@eduku.org>
2017-05-18 00:40:53 +0800

    update

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const configPath = path.join(__dirname, '../../firebase-config.json');
 
+console.log('process.env: ', process.env);
+
 admin.initializeApp({
   credential: admin.credential.cert(process.env.FIREBASE),
   databaseURL: 'https://thefastlife-571fa.firebaseio.com',
